72f76d9dc660d34affa940b1ceca5b902caa172f
vignesh <2300032475@kluniversity.in>
2025-07-30 10:43:37 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './style.css';
 function App() {
   return (
     <div className="container">
-      <h1>Welcome to My React Project - S105</h1>
+      <h1>CI/CD and Cloud Devops Course</h1>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
     </div>
   );
