9703b350274373f7c319ac900ef83a9fe2a2df71
vignesh <2300032475@kluniversity.in>
2025-07-30 10:17:16 +0530

    react project v1.0

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My React Project - S105</h1>
+      <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
+    </div>
+  );
+}
+
+export default App;
