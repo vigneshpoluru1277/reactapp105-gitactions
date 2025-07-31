4e1eeec0aab6e4fb72f206805eb079b3561e8e76
vignesh <2300032475@kluniversity.in>
2025-07-31 11:53:03 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className="container">
       <h1>Welcome to My React Project-S105</h1>
       <h1>CI/CD and Cloud Devops Course</h1>
-      <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
+      <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
     </div>
   );
 }
